dc24cdbced6e7e90321b1734dc50b6e8839f66d6
jiangros <rfh.jiang@mail.utoronto.ca>
2024-09-16 18:59:09 -0400

    Tree length()

--- src/Tree.java
+++ src/Tree.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 
-    private Integer root;               // The item stored at this tree's root, or None if the tree is empty.
+    private Integer root;               //? The item stored at this tree's root, or None if the tree is empty.
     private ArrayList<Tree> subtrees;   // The list of all subtrees of this tree.
 
     public Tree(Integer root, ArrayList<Tree> subtrees) {
@@ -17,6 +17,18 @@ public class Tree {
     }
     public boolean is_empty() {
         return this.root == null;
+    }
+
+    public int length() {
+        if (is_empty()) {
+            return 0;
+        }
+
+        int size = 1;
+        for (Tree subtree : subtrees) {
+            size += subtree.length();
+        }
+        return size;
     }
 
 }
