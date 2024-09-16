7467b5058d32c324796548a4e0028491c0795869
jiangros <rfh.jiang@mail.utoronto.ca>
2024-09-16 18:32:43 -0400

    Tree constructors and class variables

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
-    //something
+
+    private Integer root;               // The item stored at this tree's root, or None if the tree is empty.
+    private ArrayList<Tree> subtrees;   // The list of all subtrees of this tree.
+
+    public Tree(Integer root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    public Tree(Integer root) {
+        this.root = root;
+        this.subtrees = null;
+    }
+
 }
