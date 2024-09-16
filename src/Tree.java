51a492e6831298770e985c009520b75c9836dbde
Rohit Kumar <kumar@LAPTOP-9RN78FKU.ateqdirectory.ateq.ca>
2024-09-16 18:41:51 -0400

    hello-is empty

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,7 @@
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private Integer root;
+    public Boolean is_empty() {
+        return this.root == null;
+    }
 }
